da6b0e66cd6821819ba2687f92b260757b6fb663
Justin Gardner <justin@rhynorater.com>
2024-06-20 06:04:50 -0500

    3.7.6

--- wp-all-import/plugin.php
+++ wp-all-import/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: WP All Import
 Plugin URI: https://www.wpallimport.com/wordpress-xml-csv-import/?utm_source=import-plugin-free&utm_medium=wp-plugins-page&utm_campaign=upgrade-to-pro
 Description: The most powerful solution for importing XML and CSV files to WordPress. Create Posts and Pages with content from any XML or CSV file. A paid upgrade to WP All Import Pro is available for support and additional features.
-Version: 3.7.4
+Version: 3.7.6
 Author: Soflyy
 */
 
@@ -25,7 +25,7 @@ define('WP_ALL_IMPORT_ROOT_URL', rtrim(plugin_dir_url(__FILE__), '/'));
  */
 define('WP_ALL_IMPORT_PREFIX', 'pmxi_');
 
-define('PMXI_VERSION', '3.7.4');
+define('PMXI_VERSION', '3.7.6');
 
 define('PMXI_EDITION', 'free');
 
@@ -116,7 +116,7 @@ final class PMXI_Plugin {
      */
     public static $is_php_allowed = true;
 
-	public static $capabilities = 'install_plugins';
+	public static $capabilities = 'setup_network';
 
 	/**
 	 * WP All Import logs folder
@@ -240,7 +240,7 @@ final class PMXI_Plugin {
 	 */
 	protected function __construct() {
 
-        if(!is_multisite() || defined('WPAI_WPAE_ALLOW_INSECURE_MULTISITE') && 1 === WPAI_WPAE_ALLOW_INSECURE_MULTISITE){
+        if(defined('WPAI_WPAE_ALLOW_INSECURE_MULTISITE') && 1 === WPAI_WPAE_ALLOW_INSECURE_MULTISITE){
             self::$capabilities = 'manage_options';
         }
 
@@ -591,7 +591,7 @@ final class PMXI_Plugin {
 			if (method_exists($controllerName, $actionName)) {
 
 				@ini_set("max_input_time", PMXI_Plugin::getInstance()->getOption('max_input_time'));
-				@ini_set("max_execution_time", PMXI_Plugin::getInstance()->getOption('max_execution_time'));
+				@ini_set("max_execution_time", str_replace('-1','0',PMXI_Plugin::getInstance()->getOption('max_execution_time')));
 
 				if ( ! get_current_user_id() or ! current_user_can( self::$capabilities )) {
 				    // This nonce is not valid.
